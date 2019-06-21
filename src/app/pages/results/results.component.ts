23c858f79f8f79628e390c8026e1316c23834579
ktchubinsky <ktchubinsky@softland.com.ar>
2019-06-21 17:10:25 -0300

    Se agrega logica de oktonavigate en store

--- src/app/pages/results/results.component.ts
+++ src/app/pages/results/results.component.ts
@@ -15,6 +15,7 @@ export class ResultsComponent implements OnInit {
   partes:Parte[];
   storeSubscription:Subscription;
   partespaginados:Parte[];
+  paginas:any[] = [];
 
 
   constructor(public store:Store<AppState>) {
@@ -27,8 +28,13 @@ export class ResultsComponent implements OnInit {
   initResults(){
     this.storeSubscription = this.store.select('cargaresults').subscribe( data =>{
       this.partes = data.parte;
+      this.paginas = this.calculoPaginaciones();
       this.partespaginados = this.recuperarPartes(1);
     })
+  }
+
+  cambiodePagina(pagina:number){
+    this.partespaginados =  this.recuperarPartes(pagina);
   }
 
   recuperarPartes(pagina:number){
@@ -36,20 +42,41 @@ export class ResultsComponent implements OnInit {
     let parteinicial:number;
     let topepagina:number = 5;
     let partespaginados:Parte[] = [];
-    let i:number = 1
+    let i:number = 0
 
-    parteinicial = pagina*topepagina-(topepagina-1)
+    parteinicial = pagina*topepagina-(topepagina)
     i = parteinicial
-    console.log(i);
-    do{
-      partespaginados.push(this.partes[i]);
-      i = i + 1
 
+    do{
+      if (this.partes[i] !== undefined){
+          partespaginados.push(this.partes[i]);
+      }
+      i = i + 1
     }
-    while(i<=topepagina);
+    while(i<topepagina*pagina);
 
     return partespaginados;
   }
+
+  calculoPaginaciones() {
+    let cantidadpartes:number = this.partes.length
+    let topepagina:number = 5
+    let paginas:any[] = []
+    let i:number;
+    let paginasagregadas:number = 0;
+
+    paginas.push(1);
+    paginasagregadas = 1
+
+    for (i = 1; i <= cantidadpartes; i++) {
+      if (i%topepagina == 0) {
+        paginasagregadas = paginasagregadas + 1
+        paginas.push(paginasagregadas);
+      }
+    }
+
+    return paginas
+  };
   modificarOktonavigate(){
     this.store.dispatch(new ModificarOkToNavigate(false));
   }
